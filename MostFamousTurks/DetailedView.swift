59fcf980151a5b3b4da45c4b1466ab7086698bea
berkangzgn <bgezgin@protonmail.com>
2022-01-21 20:33:28 +0300

    View details added

--- MostFamousTurks/DetailedView.swift
+++ MostFamousTurks/DetailedView.swift
@@ -12,11 +12,24 @@ struct DetailedView: View {
     var body: some View {
         VStack {
             mapView(loc: selectedLeader.locCoord)
-                .frame(height: UIScreen.main.bounds.height*0.25)
+                .frame(height: UIScreen.main.bounds.height*0.3)
+                .edgesIgnoringSafeArea(.top) // Çentiğin yanları güvenli alan olarak geçiyor. Oraları da dolduruyoruz.
             
             picView(image: Image(selectedLeader.picName))
-                .frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height*0.3, alignment: .center)
+                .frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height*0.25, alignment: .center)
                 .offset(y: UIScreen.main.bounds.height * -0.15)
+        
+            Text(selectedLeader.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.043, saturation: 1.0, brightness: 0.878))
+                .offset(y: UIScreen.main.bounds.height * -0.1)
+            Text(selectedLeader.dt)
+                .foregroundColor(Color.gray)
+                .offset(y: UIScreen.main.bounds.height * -0.1)
+            Text(selectedLeader.desc)
+                .frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height*0.3, alignment: .center)
+                .offset(y: UIScreen.main.bounds.height * -0.1)
         }
     }
 }
@@ -26,3 +39,5 @@ struct DetailedView_Previews: PreviewProvider {
         DetailedView(selectedLeader: atilla)
     }
 }
+
+// Spacer() ile sayfa üzerinde yazılar arasında boşluk bıraktırabiliriz.
