2325ab70ad7fff36a2f8ac73ece54cbc498f4ea2
berkangzgn <bgezgin@protonmail.com>
2022-01-21 15:14:11 +0300

    Initial Commit

---
+++ MostFamousTurks/MostFamousTurksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostFamousTurksApp.swift
+//  MostFamousTurks
+//
+//  Created by Berkan Gezgin on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MostFamousTurksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
